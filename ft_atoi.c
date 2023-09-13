18892eaf8f6fd8d57ea2aa8f618a5df59cd2c78e
kpapa <kpapa@student.42.fr>
2023-09-13 15:42:35 +0200

    final

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpapa <kpapa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 11:53:16 by kpapa             #+#    #+#             */
+/*   Updated: 2023/05/23 21:49:27 by kpapa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	count;
+	int	val;
+	int	sign;
+
+	count = 0;
+	val = 0;
+	sign = 1;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (str[count] && str[count] >= 48 && str[count] <= 57)
+	{
+		val = (val * 10) + (str[count] - 48);
+		count ++;
+	}
+	return (val * sign);
+}
